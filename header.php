a0b204d160f4b7722a8a80ae38f9afd5c0fe7edf
erikthegamer1242 <erikbraun1242@gmail.com>
2026-06-14 15:04:41 +0200

    Added nav

---
+++ header.php
@@ -0,0 +1,21 @@
+<?php
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Sopitas</title>
+</head>
+<body>
+    <header class="header">
+        <img src="/img/logo.webp" alt="Sopitas Logo" class="logo">
+        <nav>
+            <a href="index.php">Početna</a>
+            <a href="music.php">Muzika</a>
+            <a href="sport.php">Sport</a>
+            <a href="admin.php">Administracija</a>
+        </nav>
+    </header>';
+
+?>
